ee926b7d6e245d92d7a2e8ccf8545493ee26777d
ensar <ensar.makas@gmail.com>
2023-10-27 19:52:35 +0300

    OrganizationForm now creates and changes organization

--- src/components/Forms/OrganizationForm.tsx
+++ src/components/Forms/OrganizationForm.tsx
@@ -4,15 +4,15 @@ import { Dispatch, SetStateAction, useState } from "react";
 
 const OrganizationForm = ({organization, onChange}: {organization?: Organization, onChange:  (org: Organization) => void}) => {
 	const [org, setOrg] = useState(organization ? organization : {} as Organization);
-
+	function updateOrg(organization: Organization){
+		setOrg(organization)
+		onChange(organization);
+	}
 	return <>
-		<Input type="text" isClearable placeholder="Kurum Adı" value={organization?.name} onValueChange={(value)=> {
-			setOrg({...org, name: value})
-			onChange(org)
-		}}/>
-		<Input type="text" isClearable placeholder="Kurum E posta adresi" value={organization?.email}/>
-		<Input type="text" isClearable placeholder="Kurum Adı" value={organization?.phone}/>
-		<Input type="text" isClearable placeholder="Kurum Adı" value={organization?.address}/>
+		<Input type="text" className="my-2" isClearable placeholder="Kurum Adı" value={organization?.name} onValueChange={(value)=> updateOrg({...org, name: value})}/>
+		<Input type="text" className="my-2" isClearable placeholder="Kurum Telefon Numarası" value={organization?.phone} onValueChange={(value)=> updateOrg({...org, phone: value})}/>
+		<Input type="text" className="my-2" isClearable placeholder="Kurum E posta adresi" value={organization?.email} onValueChange={(value)=> updateOrg({...org, email: value})}/>
+		<Input type="text" className="my-2" isClearable placeholder="Adres" value={organization?.address} onValueChange={(value)=> updateOrg({...org, address: value})}/>
 	</>
 }
 
