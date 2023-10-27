7b88cba8f48965058f9f50f293dde09c506dd3bb
ensar <ensar.makas@gmail.com>
2023-10-27 19:53:00 +0300

    ClientForm creates and changes client. But needs upgrades

--- src/components/Forms/ClientForm.tsx
+++ src/components/Forms/ClientForm.tsx
@@ -8,18 +8,22 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Dispatch, SetStateAction, useState } from "react"
 import { OrganizationForm } from "./OrganizationForm";
 
-const ClientForm = ({client, setClient}: {client: Client, setClient: Dispatch<SetStateAction<Client>>}) => {
-	const [currentClient, setCurrentClient] = useState(client);
-	const [isNewCompany, setIsNewCompany] = useState(false)
+const ClientForm = ({client, onClientChange}: {client?: Client, onClientChange: (client: Client) => void}) => {
+	const [currentClient, setCurrentClient] = useState(client ? client : {} as Client);
+	const [isNewCompany, setIsNewCompany] = useState(false);
+	function updateClient(client: Client){
+		setCurrentClient(client);
+		onClientChange(client);
+	}
 	return <>
 	
 		<div className="grid gap-4 grid-cols-2 my-2">
-			<Input type="text" placeholder="İsim" isClearable onValueChange={name => setCurrentClient({...currentClient, name})}/>
-			<Input type="text" placeholder="Soyisim" isClearable onValueChange={surname => setCurrentClient({...currentClient, surname})}/>
+			<Input type="text" placeholder="İsim" isClearable onValueChange={name => updateClient({...currentClient, name})}/>
+			<Input type="text" placeholder="Soyisim" isClearable onValueChange={surname => updateClient({...currentClient, surname})}/>
 		</div>
 		<div className="grid gap-4 grid-cols-2 my-2">
-			<Input type="text" placeholder="E posta" isClearable onValueChange={name => setCurrentClient({...currentClient, name})}/>
-			<Input type="phone" placeholder="5xxxxxxxxx" isClearable onValueChange={name => setCurrentClient({...currentClient, name})}/>
+			<Input type="text" placeholder="E posta" isClearable onValueChange={name => updateClient({...currentClient, name})}/>
+			<Input type="phone" placeholder="5xxxxxxxxx" isClearable onValueChange={name => updateClient({...currentClient, name})}/>
 		</div>
 		<Switch color="primary" onChange={e=> setIsNewCompany(!isNewCompany)}>
 			Yeni Ekle
@@ -36,7 +40,7 @@ const ClientForm = ({client, setClient}: {client: Client, setClient: Dispatch<Se
 						ease: [0, 0.71, 0.2, 1.01]
 					}}
 				>
-					<OrganizationForm  onChange={(org) => {setCurrentClient({...currentClient,organization: org})}}/>
+					<OrganizationForm  onChange={(org) => {updateClient({...currentClient,organization: org})}}/>
 				</motion.div>
 			</>
 		): (
@@ -58,6 +62,7 @@ const ClientForm = ({client, setClient}: {client: Client, setClient: Dispatch<Se
 
 		</motion.div>
 		)}
+		<button onClick={e => console.log(currentClient)}>Kaydet</button>
 	</>
 }
 
