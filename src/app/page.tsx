077064828f19e3651981e58bdf2108afa005878e
ensar <ensar.makas@gmail.com>
2023-11-01 13:04:07 +0300

    added some events for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,19 @@ import { ClientForm } from '@/components/Forms/ClientForm'
 import { Client } from '@/types';
 export default function Home() {
 	const [client, setClient]: [Client, any] = useState({} as Client);
+	const events = [
+		{id: "1",date:new Date()},
+		{id: "1",date:new Date(2023,10,2)},
+		{id: "1",date:new Date(2023,10,5)},
+		{id: "1",date:new Date(2023,9,30)},
+		{id: "1",date:new Date(2023,10,30)}
+	]
   return (
 	<main className="min-h-screen p-24">
 		
-		<Calendar />
+		<Calendar eventDates={events} />
 
-		<ClientForm client={client} setClient={setClient}/>
+		<ClientForm client={client} onClientChange={setClient}/>
 	</main>
   )
 }
